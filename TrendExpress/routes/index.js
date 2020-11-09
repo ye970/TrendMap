556a34af08ff141d2a4ce0a6d5c380356de55b65
ye970 <ye970>
2020-11-09 17:33:45 +0900

    mysql update

--- TrendExpress/routes/index.js
+++ TrendExpress/routes/index.js
@@ -1,9 +1,36 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const mysql = require("mysql");   // mysql 모듈 require
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+// 커넥션 연결
+let client = mysql.createConnection({
+  user: "root",
+  password: "Gachon123!@",
+  database: "MYTMAP"
+})
 
 module.exports = router;
+
+router.get('', function(req, res, next) {
+  client.query("SELECT * FROM users;", function(err, result, fields){
+    if(err){
+      console.log(err);
+      console.log("쿼리문에 오류가 있습니다.");
+    }
+    else{
+      res.render('', {
+        results: result
+      });
+    }
+  });
+});
+
+router.post('/', function(req, res, next) {
+  var body = req.body;
+
+  client.query("INSERT INTO users (uid, content, score) VALUES (?, ?, ?)", [
+    body.uid, body.content, body.score
+  ], function(){
+    res.redirect("/");
+  });
+});
